4f95993f93ec61dfc936428f919378434f373667
shoksdev <138390299+shoksdev@users.noreply.github.com>
2023-10-18 13:45:47 +0700

    Create rent, transport and payment apps

---
+++ volga_it/transports/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Transports(models.Model):
+    TRANSPORT_TYPES = [
+        ('C', 'Car'),
+        ('B', 'Bike'),
+        ('S', 'Scooter')
+    ]
+    canBeRented = models.BooleanField(default=False, verbose_name='Можно ли арендовать транспорт?')
+    transportType = models.CharField(max_length=10, choices=TRANSPORT_TYPES, verbose_name='Тип транспорта')
+    model = models.CharField(max_length=100, verbose_name='Модель транспорта')
+    color = models.CharField(max_length=50, verbose_name='Цвет транспорта')
+    identifier = models.CharField(max_length=20, verbose_name='Номерной знак')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    latitude = models.DecimalField(default=0, verbose_name='Географическая широта местонахождения транспорта')
+    longitude = models.DecimalField(default=0, verbose_name='Географическая долгота местонахождения транспорта')
+    minutePrice = models.DecimalField(default=0, blank=True, null=True, verbose_name='Цена аренды за минуту')
+    dayPrice = models.DecimalField(default=0, blank=True, null=True, verbose_name='Цена аренды за сутки')
--- volga_it/volga_it/settings.py
+++ volga_it/volga_it/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'transports.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'volga_it.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'volga_it.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'volga_it_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Test1234',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 

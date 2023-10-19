a317fec567e5c36b081306c4a1a23af96244b8f9
shoksdev <138390299+shoksdev@users.noreply.github.com>
2023-10-19 10:50:35 +0700

    makemigrations

---
+++ volga_it/rent/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Rent(models.Model):
+    TRANSPORT_TYPES = [
+        ('A', 'All'),
+        ('C', 'Car'),
+        ('B', 'Bike'),
+        ('S', 'Scooter')
+    ]
+    lat = models.DecimalField(default=0, max_digits=4, decimal_places=4,
+                              verbose_name='Географическая широта местонахождения транспорта')
+    long = models.DecimalField(default=0, max_digits=4, decimal_places=4,
+                               verbose_name='Географическая долгота местонахождения транспорта')
+    radius = models.DecimalField(default=0, max_digits=4, decimal_places=4,
+                                 verbose_name='Радиус круга поиска транспорта')
+    type = models.CharField(max_length=10, choices=TRANSPORT_TYPES, verbose_name='Тип транспорта')

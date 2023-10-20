adb185e3ee7baccfc7c348aaf71294c0f5cc717c
shoksdev <138390299+shoksdev@users.noreply.github.com>
2023-10-20 18:47:27 +0700

    Fix urls

--- volga_it/transports/urls.py
+++ volga_it/transports/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from .views import TransportListCreateView, TransportRetrieveUpdateDestroyView
 
 urlpatterns = [
-    path('transports/', TransportListCreateView.as_view(), name='transport-list'),
-    path('transports/<int:pk>/', TransportRetrieveUpdateDestroyView.as_view(), name='transport-object'),
+    path('transport/', TransportListCreateView.as_view(), name='transport-list'),
+    path('transport/<int:pk>/', TransportRetrieveUpdateDestroyView.as_view(), name='transport-object'),
 ]

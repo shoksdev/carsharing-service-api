08eb1a01884fc390aca066ceb8619a6879ee118f
shoksdev <138390299+shoksdev@users.noreply.github.com>
2023-10-30 18:06:45 +0700

    Fix Docker

--- volga_it/config/settings.py
+++ volga_it/config/settings.py
@@ -140,7 +140,7 @@ DJOSER = {
     'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
     'USERNAME_RESET_CONFIRM_URL': '#/username/reset/confirm/{uid}/{token}',
     'ACTIVATION_URL': '#/activate/{uid}/{token}',
-    'SEND_ACTIVATION_EMAIL': False,  # Эту переменную нужно изменить на True
+    'SEND_ACTIVATION_EMAIL': False,
     'SERIALIZERS': {},
 }
 
